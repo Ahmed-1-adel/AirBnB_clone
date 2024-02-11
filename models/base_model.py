5a663895b2f9c45cdb61ca9659c721b649410365
Ahmed-1-adel <Ahmedadel123321m@gamil.com>
2024-02-11 14:43:26 +0200

    Hello

--- models/base_model.py
+++ models/base_model.py
@@ -2,8 +2,9 @@
 """Defines the BaseModel class.
   And Main Varables
 """
-import uuid , datetime, sys , models
-dateTime = datetime.datetime
+import models
+from uuid import uuid4
+from datetime import datetime
 class BaseModel() :
     """This Class To Defines all Common Attribute & Methods for other classes"""
     def __init__(self, *args, **kwargs):
@@ -14,19 +15,19 @@ class BaseModel() :
             **kwargs (dict): Key/value pairs of attributes.
         """
         newData = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid.uuid4())
-        self.created_at = dateTime.now()
-        self.updated_at = dateTime.now()
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
         if len(kwargs) != 0:
             for key, value in kwargs.items():
                 if key == "created_at" or value == "updated_at":
-                    self.__dict__[key] = dateTime.strptime(value, newData)
+                    self.__dict__[key] = datetime.strptime(value, newData)
                 else :
                     self.__dict__[key] = value
         else:
             models.storage.new(self)
     def safe(self):
-        self.updated_at = dateTime.today()
+        self.updated_at = datetime.today()
         models.storage.save()
     def to_dict(self):
         """Return the dictionary of the BaseModel instance.
